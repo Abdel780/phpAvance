a23c61585c8dd36a87dfc5619ec2b4ee2dea43e4
Abdel780 <abdelkarim27200@gmail.com>
2024-09-09 14:40:10 +0200

    creation dossier maj exo

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -23,7 +23,13 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
+                        <a class="nav-link active" href="/app/Views/requetes.view.php">Les requetes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/app/Views/get.view.php">Le GET</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/app/Views/traitementexo.view.php">Exo</a>
                     </li>
                 </ul>
             </div>
